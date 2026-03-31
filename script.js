566a41b246318c62cdf97afb66996ad4607679ac
Altair <87874075+Magiper@users.noreply.github.com>
2026-03-31 13:25:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -188,12 +188,35 @@ function renderResults(query){
     document.getElementById("results").innerHTML = html;
 }
 
+function renderLawResults(query){
+    let results = lawDatabase.filter(item =>
+        item.keywords.some(k =>
+            k.toLowerCase().includes(query.toLowerCase())
+        )
+    );
+
+    let html = "";
+
+    results.forEach(r => {
+        html += `<div class="result-card">`;
+        html += `<div class="term">${r.law}</div>`;
+        html += `<strong>${r.article}</strong>`;
+
+        html += `<p><b>English:</b><br>${r.english}</p>`;
+        html += `<p><b>Indonesian:</b><br>${r.indonesian}</p>`;
+
+        html += `</div>`;
+    });
+
+    document.getElementById("lawResults").innerHTML = html;
+}
+
 function selectLaw(keyword){
     let input = document.getElementById("lawSearch");
     input.value = keyword;
     document.getElementById("lawSuggestions").innerHTML = "";
 
-    input.dispatchEvent(new Event("input"));
+    renderLawResults(keyword);
 };
 
 function speak(text){
